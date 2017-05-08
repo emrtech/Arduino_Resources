df6428bd14a3a713c1e567d87d2f2b41f5604bab
Gerhard Oberforcher <emrtech1@interbaun.com>
2017-05-07 21:20:15 -0600

    Modified Arduino core serial libraries for RS485
    Initial commit of modified Arduino core Serial Libary. Please refer to Readme.txt for installation information.

---
+++ RS485_Send_Example.ino
@@ -0,0 +1,59 @@
+/*
+ * MAY 08, 2017 by GJO
+ * 
+ * You may use this program freely in any way without any restrictions
+ * 
+ * This is a simple test program to verify the RS485 hardware functionality.
+ * Send any data to the uC and it will echo back the sent character while
+ * actuationg the RS485 DIR send enable line. 
+ * 
+ * It was tested with a PRO-MINI and a RS485 adapter board from eBay uilizing a 
+ * MAX485 transceiver. The RS485 port of this interface was conencted to a USB to RS485 converter
+ * and TERA TERM for communciation on a PC at 9600 baud.
+ * 
+ * RS485 Adapter connected to Pro-Mini pins:
+ * 
+ * RO --------> RXI PIN
+ * DE = DI ---> D8 IO Pin 
+ * DI <-------- TXO-PIN
+ * 
+ * If you choose to keep the FTDI USB programming adapter to the Pro-Mini
+ * you must put a diode in series with the RO pin of the MAX485 transceiver to prevent 
+ * a short circuit. This way you may communicate via the USB serial adapter as well as 
+ * the RS485 transceiver, but not at the same time. Connect Anode of 1N4148 to RXI pin 
+ * of Pro-Mini and the cathode to the  * RO pin of the NAX485. 
+ * 
+ * If for some reason you need an active low signal, change RS485_ACTIVE_HIGH to RS485_ACTIVE_LOW
+ * 
+ * 
+ */
+
+
+
+#define BAUD_USART 9600
+#define RS485_ACTIVE_HIGH 0
+#define RS485_ACTIVE_LOW 1
+#define RS485DIR_PIN 8
+#define RS485_ENABLED 1
+
+char c = 0;   // for incoming serial data
+
+void setup() {
+// initialize serial with RS485 transceiver
+Serial.begin(BAUD_USART, SERIAL_8N1, RS485_ENABLED, RS485DIR_PIN, RS485_ACTIVE_HIGH);
+
+}
+
+void loop() {
+
+  // send data only when you receive data:
+  if (Serial.available() > 0) {
+    // read the incoming character
+    c = Serial.read();
+    // send character back to sender
+    Serial.print(c);
+  }
+}
+
+
+
---
+++ Readme.txt
@@ -0,0 +1,33 @@
+V1.00 2017-MAY-08
+-----------------
+Instructions for use of RS485 modified core Serial library:
+
+Simply replace the existing Arduino core Serial library files with the submitted files in this location:
+
+(Example of my setup:)
+
+C:\Program Files (x86)\Arduino_V182\hardware\arduino\avr\cores\arduino\HardwareSerial.cpp
+C:\Program Files (x86)\Arduino_V182\hardware\arduino\avr\cores\arduino\HardwareSerial.h
+
+(Note: You may need to make sure you have administrator permissions to replace those files.)
+
+If this is a problem download the ARDUINO ZIPPED Non-Admin version, unzip it and 
+replace the Serial.Lib files there before you use the IDE.
+
+You may keep the original files in a safe place for safe keeping, but they must be removed from the folder.
+
+Make sure Arduino IDE is not running.
+
+A simple example test program is included to test your RS485 setup.
+
+The IDE should probaby no earlier the V1.65. It was tested with the current IDE version V1.82
+
+The program was tested with:
+
+ARDUINO PRO-MINI CLONE running at 8MHz
+RS485 transceiver board from eBay utilizing a MAX485 transceiver IC (Refer also to example program comments)
+PRo-MINI IO LINE D8 was used for DIR control, TXI/TXO for serial data connection
+USB to RS485 adapter utilizing FTDI232RL and MAX483 transceiver. Terminated in 120 Ohm
+TERATERM terminal program at 9600 baud
+FTDI based USB serial cable to program Pro-Mini utilizing boot loader
+------------------------------------------------------------------------------------------------------------
